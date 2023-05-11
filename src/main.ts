ac4afe01dae634712eec852f49554b639e2e3a3f
Brian Michalski <bmichalski@gmail.com>
2023-05-10 23:48:05 -0400

    Get videos, at least ones that exist.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,46 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+async function getVideo(key: string, address: string) {
+  const response = await fetch(`https://aerialview.googleapis.com/v1beta/videos?address=${encodeURIComponent(address)}&key=${key}`);
+  return response.json();
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function getVideoClickHandler() {
+  const apiKeyElem = document.getElementById('apikey')! as HTMLInputElement;
+  const addrElem = document.getElementById('address')! as HTMLInputElement;
+
+  const apiKey = apiKeyElem.value;
+  const address = addrElem.value;
+
+  const response = await getVideo(apiKey, address);
+  
+  
+  if (response.state === 'ACTIVE') {
+    document.getElementById('response')!.innerHTML = `
+    Video ID: ${response.metadata.videoId}
+    Duration: ${response.metadata.duration}
+    <table class="table-auto">
+      <thead>
+      <tr>
+        <th>Resource</th>
+        <th>Preview</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr><td>IMAGE.landscapeUri</td><td><img src="${response.uris.IMAGE.landscapeUri}"/></td></tr>
+      <tr><td>IMAGE.portraitUri</td><td><img src="${response.uris.IMAGE.portraitUri}"/></td></tr>
+      <tr><td>MP4_HIGH.landscapeUri</td><td><video src="${response.uris.MP4_HIGH.landscapeUri}" controls/></td></tr>
+      <tr><td>MP4_HIGH.portraitUri</td><td><video src="${response.uris.MP4_HIGH.portraitUri}" controls/></td></tr>
+      <tr><td>MP4_MEDIUM.landscapeUri</td><td><video src="${response.uris.MP4_MEDIUM.landscapeUri}" controls/></td></tr>
+      <tr><td>MP4_MEDIUM.portraitUri</td><td><video src="${response.uris.MP4_MEDIUM.portraitUri}" controls/></td></tr>
+      <tr><td>MP4_LOW.landscapeUri</td><td><video src="${response.uris.MP4_LOW.landscapeUri}" controls/></td></tr>
+      <tr><td>MP4_LOW.portraitUri</td><td><video src="${response.uris.MP4_LOW.portraitUri}" controls/></td></tr>
+    </tbody>
+    </table>
+    `
+  } else {
+    console.log("We have a problem");
+  }
+}
+
+document.getElementById('getVideo')!.addEventListener('click', getVideoClickHandler);
